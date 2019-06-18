a9844986ed2bd224cf953aff38bf7af3541633d7
k-matusm <k-matusm@sel-samba.ics.es.osaka-u.ac.jp>
2019-06-18 17:55:59 +0900

    add sequential version

--- parallel_monte_carlo.cpp
+++ parallel_monte_carlo.cpp
@@ -2,7 +2,7 @@
 #include <random>
 #include <iomanip>
 #include <numeric>
-#include <mutex>
+#include <chrono>
 #include <omp.h>
 
 
@@ -13,55 +13,69 @@ public:
 		: mt(std::random_device()())
 	{}
 
-	double yeild() noexcept
+	auto yeild() noexcept
 	{
 		return std::generate_canonical<double, std::numeric_limits<double>::digits>(this->mt);
 	}
 
 private:
-	std::mt19937 mt;
+	std::mt19937_64 mt;
 };
 
-class parallel_monte_carlo final
+class monte_carlo final
 {
 public:
-	double calc(const int all, const int granularity)
-	{
-		std::vector<int> sample_set;
+	explicit monte_carlo() noexcept = default;
 
-		int rest = all;
-		for (; rest > granularity; rest -= granularity)
-		{
-			sample_set.push_back(granularity);
-		}
-		sample_set.push_back(rest);
+	auto parallel_calc(const int n_samples) const noexcept
+	{
+		std::vector<int> sample_set(omp_get_max_threads(), 0);
+		int rest = n_samples;
+		const int granularity = n_samples / omp_get_max_threads() + 1;
+		std::for_each(sample_set.begin(), sample_set.end(), [&](auto& v) mutable { v = std::min(rest, granularity); rest -= granularity; });
 
 		int in = 0;
-		std::mutex iomutex;
 #pragma omp parallel for reduction(+:in)
 		for (int i=0; i<sample_set.size(); ++i)
 		{
-			in += this->count(sample_set[i], iomutex);
+			in += this->count(sample_set[i]);
 		}
-		return (in / double(all)) * 4;
+		return (in / double(n_samples)) * 4;
+	}
+
+	auto sequential_calc(const int n_samples) const noexcept
+	{
+		return (this->count(n_samples) / double(n_samples)) * 4;
 	}
 
 private:
-	int count(const int n_samples, std::mutex &iomutex)
+	int count(const int n_samples) const noexcept
 	{
 		random_generator rg;
 		std::vector<std::pair<double, double>> samples(n_samples);
 		std::generate(samples.begin(), samples.end(), [&]() { return std::make_pair(rg.yeild(), rg.yeild()); });
-		int in=std::count_if(samples.begin(), samples.end(), [](const auto& p) { return std::pow(p.first, 2) + std::pow(p.second, 2) <= 1; });
-
-		std::lock_guard<std::mutex> lock(iomutex);
-		std::cout << std::setprecision(std::numeric_limits<double>::max_digits10) << "In thread id=" << omp_get_thread_num() << ", " << in << " of " << n_samples << " sample(s) hit inside the circle." << std::endl;
-		return in;
+		return std::count_if(samples.begin(), samples.end(), [](const auto& p) { return std::pow(p.first, 2) + std::pow(p.second, 2) <= 1; });
 	}
 };
 
+
 int main()
 {
-	std::cout << "result: " << parallel_monte_carlo().calc(100000000, 100000) << std::endl;
+	constexpr int n_samples = 1000;
+	const auto pstart = std::chrono::system_clock::now();
+	const auto pv = monte_carlo().parallel_calc(n_samples);
+	const auto pend = std::chrono::system_clock::now();
+	std::cout << "parallel:" << std::endl;
+	std::cout << "\tresult: " << pv << std::endl;
+	std::cout << "\ttime: " << std::chrono::duration_cast<std::chrono::milliseconds>(pend - pstart).count() << "ms" << std::endl;
+
+	const auto sstart = std::chrono::system_clock::now();
+	const auto sv = monte_carlo().sequential_calc(n_samples);
+	const auto send = std::chrono::system_clock::now();
+
+	std::cout << std::endl;
+	std::cout << "sequential:" << std::endl;
+	std::cout << "\tresult: " << sv << std::endl;
+	std::cout << "\ttime: " << std::chrono::duration_cast<std::chrono::milliseconds>(send - sstart).count() << "ms" << std::endl;
 
 }
